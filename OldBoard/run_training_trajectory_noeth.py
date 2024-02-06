c565bc3fb106dc76965fec6055ee0c3aa23256c2
Sharmi <sharmis@mit.edu>
2024-02-06 11:16:21 -0500

    changed printing

--- OldBoard/run_training_trajectory_noeth.py
+++ OldBoard/run_training_trajectory_noeth.py
@@ -258,12 +258,10 @@ class TrainingRobotController:
 
             # wait for a response, then perform logging
             while 1:
-           # could check to make sure dynamixels are in their position before sampling, but should be okay
+            # could check to make sure dynamixels are in their position before sampling, but should be okay
                 # log data
-                    # could clear ethernet messages here with the while loop used in main()
                 if j>=0:
                     # record data
-                    
                     num_data_points = 10
                     for i in range(num_data_points):
                         
@@ -277,7 +275,8 @@ class TrainingRobotController:
            
                         for i in range(len(dxl_ids)):
                             self.present_pos[i] = self.groupSyncRead.getData(dxl_ids[i], ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
-                            print('present pos: ', self.present_pos[i])
+                            
+                        # print('present pos: ', self.present_pos)
 
                          # Clear syncwrite parameter storage
                         self.groupSyncRead.clearParam()
@@ -313,10 +312,10 @@ class TrainingRobotController:
                         flt_data.append(float(self.traj[j-2][4])) # ATI beta des in rads
                         flt_data.append(self.present_pos[0]) # gantry x act in pulse counts
                         flt_data.append(self.present_pos[1]) # gantry y1 act in pulse counts
-                        # flt_data.append(self.present_pos[2]) # gantry y2 act in pulse counts
-                        # flt_data.append(self.present_pos[3]) # gantry z act in pulse counts
-                        # flt_data.append(self.present_pos[4]) # gantry theta act in pulse counts
-                        # flt_data.append(self.present_pos[5]) # gantry phi act in pulse counts
+                        flt_data.append(self.present_pos[2]) # gantry y2 act in pulse counts
+                        flt_data.append(self.present_pos[3]) # gantry z act in pulse counts
+                        flt_data.append(self.present_pos[4]) # gantry theta act in pulse counts
+                        flt_data.append(self.present_pos[5]) # gantry phi act in pulse counts
 
                         #TODO: add current dxl positions
                         # convert data for logging
